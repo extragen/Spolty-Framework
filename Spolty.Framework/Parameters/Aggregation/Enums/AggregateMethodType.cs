da987a3841293d5da0e9179d5c4d04896ac24430
extragen <vboretskiy@gmail.com>
2009-06-03 16:59:20 +0000

    git-svn-id: https://spolty-server/svn/spolty.framework/trunk@9 f21693ed-1c7a-344e-a3a7-1473b7fba28d

---
+++ Spolty.Framework/Parameters/Aggregation/Enums/AggregateMethodType.cs
@@ -0,0 +1,14 @@
+namespace Spolty.Framework.Parameters.Aggregations.Enums
+{
+    public enum AggregateMethodType
+    {
+        None,
+        Sum,
+        Count,
+        LongCount,
+        Min,
+        Max,
+        Average,
+        Aggregate
+    }
+}
