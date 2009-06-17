92adfc52e60d5632b472812ac0a0e6ac124c01f2
extragen <vboretskiy@gmail.com>
2009-06-17 08:59:03 +0000

    Release в отдельную папку
    git-svn-id: https://spolty-server/svn/spolty.framework/trunk@29 f21693ed-1c7a-344e-a3a7-1473b7fba28d

--- Spolty.Framework/Properties/AssemblyInfo.cs
+++ Spolty.Framework/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spolty.Framework")]
-[assembly: AssemblyDescription("Dynamic Linq query")]
+[assembly: AssemblyTitle("Spolty Framework")]
+[assembly: AssemblyDescription("Dynamic Linq Queryable")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Spolty")]
 [assembly: AssemblyProduct("Spolty.Framework")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
